72da250c22de863f02fd0636a09005bb7844e966
jomasd <jdlopes06@gmail.com>
2023-07-13 17:25:25 -0400

    Pages creation

--- artist-website/imports/ui/Router.jsx
+++ artist-website/imports/ui/Router.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage/HomePage';
+import AboutPage from './components/AboutPage/AboutPage';
+import PortfolioPage from './components/PortfolioPage/PortfolioPage';
+import ContactPage from './components/ContactPage/ContactPage';
 
 const AppRouter = () => (
   <Router>
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/about" element={<AboutPage />} />
+      <Route path="/portfolio" element={<PortfolioPage />} />
+      <Route path="/contact" element={<ContactPage />} />
       {/* Add more routes as needed */}
     </Routes>
   </Router>
---
+++ artist-website/imports/ui/components/AboutPage/AboutPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PageName = () => {
+  return (
+    <div>
+      <h1>Welcome to the PageName</h1>
+      <p>This is a brief introduction to the PageName.</p>
+      {/* Add more content as needed */}
+    </div>
+  );
+};
+
+export default PageName;
